0fb02909a749c49a2ad585c92fff307755068b1e
MaryamMosstoufi <maryam@zooxoos.com>
2020-10-01 09:45:41 -0400

    Word Count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,29 @@
 def word_count(s):
     # Your code here
-
+    dic = {}
+    excluded = '":;,.-+=/\\|[]{}()*^&'
+    if s == '':
+        return dic
+    s = s.lower()
+    s = s.replace('\r', ' ')
+    s = s.replace('\n', ' ')
+    s = s.replace('\t', ' ')
+    words = s.split(' ')
+    for word in words:
+        for char in excluded:
+            if char in word:
+                word = word.replace(char, '')
+        if word != '':
+            if word in dic:
+                dic[word] += 1
+            else:
+                dic[word] = 1
+    return dic
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
