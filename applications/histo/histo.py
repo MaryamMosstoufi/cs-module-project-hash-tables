5f1ca91513537dd09b6ff7fba2088e0a53f2477d
MaryamMosstoufi <maryam@zooxoos.com>
2020-10-01 13:06:54 -0400

    Histo

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,36 @@
 # Your code here
 
+
+def hist(file):
+    with open(file) as f:
+        s = f.read()
+    # Your code here
+    dic = {}
+    excluded = '":;,.-+=/\\|[]{}()*^&'
+    if s == '':
+        return dic
+    s = s.lower()
+    s = s.replace('\r', ' ')
+    s = s.replace('\n', ' ')
+    s = s.replace('\t', ' ')
+    words = s.split(' ')
+    for word in words:
+        for char in excluded:
+            if char in word:
+                word = word.replace(char, '')
+        if word != '':
+            if word in dic:
+                dic[word] += 1
+            else:
+                dic[word] = 1
+    dic = {k: v for k, v in sorted(
+        dic.items(), key=lambda item: item[0])}
+    dic = {k: v for k, v in sorted(
+        dic.items(), key=lambda item: item[1], reverse=True)}
+    for key in dic:
+        l = len(key)
+        space = (20 - l)*' '
+        print(key, space, dic[key]*'#')
+
+
+hist("./robin.txt")
