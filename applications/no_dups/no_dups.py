8006d8fdcd656c2e37949345cd85565078bf1986
MaryamMosstoufi <maryam@zooxoos.com>
2020-10-01 13:20:11 -0400

    No Dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,16 @@
 def no_dups(s):
     # Your code here
+    dic = {}
+    words = s.split(' ')
+    for word in words:
+        if word not in dic:
+            dic[word] = True
+    string = ""
+    for key in dic:
+        string = string + ' ' + key
+    string = string.replace(' ', '', 1)
 
+    return (string)
 
 
 if __name__ == "__main__":
